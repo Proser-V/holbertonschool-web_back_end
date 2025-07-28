83b2552ed278b010502f6fa8132ed708090e1f6d
Proser-V <10530@holbertonstudents.com00>
2025-07-28 10:35:25 +0200

    101-safely_get_value.py

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -4,12 +4,12 @@ A module for a type-annotated function safely_get_value witch takes a dict of
 variable type keys.
 """
 
-from typing import TypeVar, Any, Dict
+from typing import TypeVar, Any, Dict, Optional
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Dict[Any, T], key: Any, default: T = None) -> T:
+def safely_get_value(dct: Dict[Any, T], key: Any, default: Optional[T] = None) -> Optional[T]:
     """
     A type-annotated function safely_get_value witch takes a dict of
     variable type keys.
