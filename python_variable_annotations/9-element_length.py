89e2003b0eab8a4d259f0e954489857434a16d63
Proser-V <10530@holbertonstudents.com00>
2025-07-28 10:25:51 +0200

    9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-A module for a type-annotated function element_length witch takes a list of
-sequences.
+A module for a type-annotated function element_length witch takes an iterable
+of sequences.
 """
 
-from typing import Tuple, List, Sequence
+from typing import Tuple, List, Sequence, Iterable
 
 
-def element_length(lst: List[Sequence]) -> Tuple[Sequence, int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     A type-annotated function element_length witch takes a list of
     sequences.
