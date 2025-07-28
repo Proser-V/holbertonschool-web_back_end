846231dc74dd02727f1c26b56b32d811ba4c246d
Proser-V <10530@holbertonstudents.com00>
2025-07-28 09:38:12 +0200

    2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -7,7 +7,7 @@ string str2 as arguments and returns a concatenated string.
 """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     Type-annotated function concat that takes a string str1 and a string str2
     as arguments and returns a concatenated string.
