a01f8b23d7b42ad8e2a847ec3e216f57e6c6a3ca
Proser-V <10530@holbertonstudents.com00>
2025-07-28 10:29:56 +0200

    101-safely_get_value.py

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,7 @@ variable type keys.
 from typing import Dict, TypeVar
 
 
-def safely_get_value(dct: Dict, key: TypeVar, default = None) -> Dict[TypeVar]:
+def safely_get_value(dct: Dict, key: TypeVar, default = None) -> Dict:
     """
     A type-annotated function safely_get_value witch takes a dict of
     variable type keys.
