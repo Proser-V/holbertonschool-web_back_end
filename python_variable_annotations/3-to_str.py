21bb8b0eb7f46f14d892df65bd50bf18f72982d8
Proser-V <10530@holbertonstudents.com00>
2025-07-28 09:44:09 +0200

    3-to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -4,7 +4,7 @@ A module for a type-annotated function to_str that takes a float n as argument
 and returns the string representation of the float.
 """
 
-def floor(n: float) -> str:
+def to_str(n: float) -> str:
     """
     Type-annotated function function to_str that takes a float n as argument
     and returns the string representation of the float.
