03cb0821b96010a8a97d93867b5e3333156908ef
Proser-V <10530@holbertonstudents.com00>
2025-07-28 10:14:00 +0200

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -12,4 +12,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     Type-annotated function to_kv that takes a string k and an int OR float
     v as arguments and returns a tuple.
     """
-    return (k, v)
+    return (k, v * v)
