556c9a7f96b610a4a3f6623b019eaab614cbfb4a
Proser-V <10530@holbertonstudents.com00>
2025-07-28 12:26:53 +0200

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,13 +6,13 @@ A module for a method.
 
 import asyncio
 import random
-from typing import AsyncIterator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncIterator[float]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     The said method.
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield float(random.uniform(0, 10))
