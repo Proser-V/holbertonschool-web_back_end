770e9e2faefe81e9e96e6d04d06652cb70537b00
Proser-V <10530@holbertonstudents.com00>
2025-07-28 11:33:19 +0200

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -14,11 +14,14 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Function that wait randomly between 0 and max_delay.
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks = [wait_random(max_delay) for _ in range(n)]
     delays: List[float] = []
 
     for task in asyncio.as_completed(tasks):
         delay = await task
-        bisect.insort(delays, delay)
+        i = 0
+        while i < len(delays) and delays[i] < delay:
+            i += 1
+        delays.insert(i, delay)
 
     return delays
