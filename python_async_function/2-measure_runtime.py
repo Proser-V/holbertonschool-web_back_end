657a6fe77f113e31d9a0aa227f22e6c2c589c7b5
Proser-V <10530@holbertonstudents.com00>
2025-07-28 11:50:49 +0200

    2-measure_runtime.py

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Function that give the time delay.
     """
