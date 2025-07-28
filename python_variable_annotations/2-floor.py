e6c39402a8fa289ecba34d3c8d20db05420f9597
Proser-V <10530@holbertonstudents.com00>
2025-07-28 09:37:05 +0200

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+import math
+
+#!/usr/bin/env python3
+"""
+A module for a type-annotated function concat that takes a string str1 and a
+string str2 as arguments and returns a concatenated string.
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Type-annotated function concat that takes a string str1 and a string str2
+    as arguments and returns a concatenated string.
+    """
+    return math.floor(n)
