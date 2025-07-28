b7abc936013e2bad768a7480d48654d02f9f2d85
Proser-V <10530@holbertonstudents.com00>
2025-07-28 10:42:49 +0200

    100-safe_first_element.py

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -4,10 +4,10 @@ A module for a type-annotated function safely_get_value witch takes a dict of
 variable type keys.
 """
 
-from typing import List, Any
+from typing import Sequence, Any, Optional
 
 
-def safe_first_element(lst: List) -> Any:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """
     A type-annotated function safely_get_value witch takes a dict of
     variable type keys.
