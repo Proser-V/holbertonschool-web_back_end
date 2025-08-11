90649401d780957c75ad7936cbec56ec563b4d69
Proser-V <10530@holbertonstudents.com00>
2025-08-11 15:51:55 +0200

    12-log_stats.py

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+A module for a function that provides some stats about Nginx logs stored in
+MongoDB.
+"""
+from pymongo import MongoClient
+
+def nginx_logs():
+    """
+    Provides some stats about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient()
+    db = client["logs"]
+    collection = db["nginx"]
+
+    total_logs = collection.count_documents({})
+    print (f'{total_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({ "method": method })
+        print(f'\t method {method}: {count}')
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    nginx_logs()
