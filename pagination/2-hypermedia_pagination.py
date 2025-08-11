f42d5e2ee580ebbfea2ff7deb3a2cb623975854f
Proser-V <10530@holbertonstudents.com00>
2025-08-11 11:36:51 +0200

    2-hypermedia_pagination.py

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Module for a class and a function to paginate the dataset.
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get the page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        data = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+        return data[start_index: end_index]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        Function that return a dict with 6 keys: page_size, page, data,
+        next_page, prev_page, total_pages.
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        if page == total_pages:
+            next_page = None
+        else:
+            next_page = page + 1
+        if page - 1 == 0:
+            prev_page = None
+        else:
+            prev_page = page - 1
+
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    A function that should return a tuple of size two containing a
+    start index and an end index corresponding to the range of indexes to
+    return in a list for those particular parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
