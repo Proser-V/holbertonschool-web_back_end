f18f244ad5a3a3f3228f33039efb7de715900e4c
Proser-V <10530@holbertonstudents.com00>
2025-08-11 15:03:38 +0200

    10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A module for a function that changes all topics of a school document based on
+the name (MongoDB).
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name.
+    """
+    result = mongo_collection.update_many(
+    {"name": name},
+    {"$set": {"topics": topics}}
+)
+    return result
