16f13288089ff0bf1bdc07937ead85395540e676
Proser-V <10530@holbertonstudents.com00>
2025-08-11 15:07:08 +0200

    11-schools_by_topic.py

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A module for a function that returns the list of school having a specific
+topic (MongoDB).
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic.
+    """
+    result = mongo_collection.find_many(
+        { "topics": topic }
+    )
+    return result
