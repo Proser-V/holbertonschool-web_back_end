35dc3c0f0a5cac53fc18e5584950aa06413ddb4e
Proser-V <10530@holbertonstudents.com00>
2025-08-20 10:01:32 +0200

    3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -13,16 +13,10 @@ export default class Currency {
   }
 
   set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._name = value;
   }
 
   set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
     this._code = value;
   }
 
