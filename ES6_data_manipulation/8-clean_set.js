41639e1b70f306b57fbca806ff78d9a5103a4eda
Proser-V <10530@holbertonstudents.com00>
2025-08-25 11:59:00 +0200

    8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+    if (!startString || typeof startString !== 'string') return '';
+    
+    return [...set]
+        .filter((element) => element.startsWith(startString))
+        .map((element) => element.slice(startString.length))
+        .join('-');
+}
