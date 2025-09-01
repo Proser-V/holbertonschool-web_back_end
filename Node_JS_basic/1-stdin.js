ea58dcd01d159f0b983c61d5a0693c47cbe7418d
Proser-V <10530@holbertonstudents.com00>
2025-09-01 10:53:22 +0200

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
-process.stdin.setEncoding("utf8");
-console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on("data", (data) => {
+process.stdin.setEncoding('utf8');
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
   const input = data.trim();
-  console.log("Your name is:", input)
-  console.log("This important software is now closing");
-  process.exit(0);
-})
+  console.log('Your name is:', input);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
