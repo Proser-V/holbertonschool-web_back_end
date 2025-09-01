b9f36882d1e9fe137a7333a86a7bd7a99a8696bb
Proser-V <10530@holbertonstudents.com00>
2025-09-01 16:00:44 +0200

    6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const app = express();
+const port = 1245;
 
-app.get('/', (request, response) => {
-    response.send('Hello Holberton School!');
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
 
 module.exports = app;
