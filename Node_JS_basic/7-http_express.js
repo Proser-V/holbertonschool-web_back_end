960a0662f962c288dbf7bf313c4bd0651335ee90
Proser-V <10530@holbertonstudents.com00>
2025-09-01 16:08:07 +0200

    6-http_express.js

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School');
+});
+
+app.get('/students', (req, res) => {
+    countStudents(database)
+        .then((report) => {
+            res.send(`This is the list of our students\n${report}`);
+        })
+        .catch(() => {
+            res.send('This is the list of our students\nCannot load the database')
+        });
+});
+
+app.listen(1245);
+
+module.exports = app;
