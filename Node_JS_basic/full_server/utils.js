e80628a69d49451bc8f21737a79f0535e817d8b7
Proser-V <10530@holbertonstudents.com00>
2025-09-01 16:51:42 +0200

    task 8

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function readDatabase(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+        const lines = data
+            .trim()
+            .split('\n')
+            .filter((line) => line.trim() !== '') // removes empty lines
+        
+        const header = lines.shift().split(',');
+        const firstnameIndex = header.indexOf('firstname');
+        const fieldIndex = header.indexOf('field');
+
+        const studentsByField = {};
+
+        lines.forEach((line) => {
+            const parts = line.split(',');
+            const firstname = parts[firstnameIndex];
+            const field = parts[fieldIndex];
+
+            if (firstname && field) {
+                if (!studentsByField[field]) {
+                    studentsByField[field] = [];
+                }
+                studentsByField[field].push(firstname);
+            }
+        });
+
+        resolve(studentsByField);
+        });
+    });
+}
+
+module.exports = readDatabase;
